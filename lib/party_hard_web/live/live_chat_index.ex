f49ae41a443d2dd33482b4bdfe7b11706d90a0d0
Eduardo V. Bruno <eduardo@bruno.etc.br>
2025-08-11 23:41:02 -0300

    dedicated render / view file

--- lib/party_hard_web/live/live_chat_index.ex
+++ lib/party_hard_web/live/live_chat_index.ex
@@ -3,7 +3,6 @@ defmodule PartyHardWeb.LiveChat.Index do
   require Logger
 
   alias PartyHard.EchoServer.Message, as: ChatMessage
-  # alias PartyHard.EchoServer.History, as: ChatHistory
   alias PartyHard.EchoServer
 
   @initial_data %{"message" => nil, "delay" => 1000}
@@ -22,86 +21,10 @@ defmodule PartyHardWeb.LiveChat.Index do
      |> stream(:messages, []), temporary_assigns: [form: nil]}
   end
 
-  @impl true
-  def render(assigns) do
-    ~H"""
-    <Layouts.app flash={@flash}>
-      <.header>
-        Welcome <span class="text-2xl font-extrabold
-        subpixel-antialiased
-    ">{@current_user}</span>
-        <:subtitle>This is a echo server (in memory history)</:subtitle>
-      </.header>
-
-      <.form id="message-form" for={@form} phx-submit="send" phx-change="update_delay">
-        <.input
-          field={@form[:message]}
-          type="text"
-          label="Message"
-          autocomplete="off"
-          required
-          minlength="2"
-        />
-        <.input
-          field={@form[:delay]}
-          type="range"
-          min="0"
-          max="3000"
-          step="250"
-          label="Echo Delay"
-        />
-        <footer class="text-xs mt-2 mb-3 label mb-1">
-          (millis) {@form.params["delay"]}
-        </footer>
-        <footer class="flex justify-end gap-4">
-          <.button phx-disable-with="Sending..." variant="primary">
-            Send message
-            <.icon name="hero-paper-airplane-micro" class="size-4 opacity-75 hover:opacity-100" />
-          </.button>
-          <.button type="button" phx-click="broadcast">
-            Broadcast <.icon name="hero-globe-alt" class="size-4 opacity-75 hover:opacity-100" />
-          </.button>
-          <.button type="button" phx-click="signout">
-            Signout
-            <.icon
-              name="hero-arrow-left-start-on-rectangle"
-              class="size-4 opacity-75 hover:opacity-100"
-            />
-          </.button>
-        </footer>
-      </.form>
-
-      <div :if={@loading?}>
-        <.icon name="hero-loading" class="ml-1 size-6 motion-safe:animate-spin" />
-      </div>
-
-      <div
-        :if={@empty? and not @loading?}
-        class="flex py-3 justify-center text-sm text-base-content/70 subpixel-antialiased font-mono"
-      >
-        <p>
-          empty history <.icon name="hero-warning" class="ml-1 size-4 hero-exclamation-triangle" />
-        </p>
-      </div>
-
-      <div
-        id="chat-messages"
-        phx-update="stream"
-        class="flex flex-col gap-4"
-      >
-        <div
-          :for={{id, message} <- @streams.messages}
-          id={id}
-          class="flex flex-col p-4 rounded-box bg-base-200 shadow-md"
-        >
-          <div class="text-xs font-light">{message.date}</div>
-          <div class="font-bold">{message.sender}</div>
-          <div class="font-mono">{message.content}</div>
-        </div>
-      </div>
-    </Layouts.app>
-    """
-  end
+  # moved to index.html.heex
+  # @impl true
+  # def render(assigns) do
+  # end
 
   @impl true
   def handle_event("send", params, socket) do
